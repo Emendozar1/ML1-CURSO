b36aa8407ddc526a8b516c4e21a69f84e6fde391
Emendozar1 <esmendoza@universidadean.edu.co>
2025-06-17 10:51:40 -0500

    Delete test_sgd_classifier.py

--- test_sgd_classifier.py
+++
@@ -1,6 +0,0 @@
-
-def test_sgd_classifier_training():
-    from sklearn.linear_model import SGDClassifier
-    assert 'sgd_cls' in globals(), "No se encontró el modelo 'sgd_cls'"
-    assert isinstance(sgd_cls, SGDClassifier), "'sgd_cls' no es una instancia de SGDClassifier"
-    assert hasattr(sgd_cls, 'coef_'), "El modelo 'sgd_cls' no ha sido entrenado correctamente"
